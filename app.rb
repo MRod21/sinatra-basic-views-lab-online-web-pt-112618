20c536d8d6e59876ac09a06bc0840963ae3bc293
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-03-19 23:43:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
   get "/" do
+=======
+  get "/index" do
+>>>>>>> 0cbed4249b901bef3868dc3e1d0be376e7db438a
     erb :index
   end
   
